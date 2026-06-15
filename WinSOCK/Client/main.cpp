cbf6486cf2eec4d3c8772fb62066a1fd8c98ce7f
Максим <@DESKTOP-VML76U3>
2026-06-15 14:08:06 +0300

    Server closing problem solved

--- WinSOCK/Client/main.cpp
+++ WinSOCK/Client/main.cpp
@@ -154,10 +154,15 @@ VOID Recieve(SOCKET connect_socket)
 		cout << FormatLastError(dwError, szError) << endl;*/
 
 		if (iResult > 0)cout << recvbuffer << "(" << iResult << " Bytes)" << endl;
-		//else if (result == 0) cout << "Connection closed" << endl;
-		else	cout << FormatLastError(WSAGetLastError(), szError) << endl;
-		cout << "Receive failed:\t" << WSAGetLastError() << endl;
-		} while (strcmp(recvbuffer, DECLINE_MESSAGE) != 0);
+		else if (iResult == 0) cout << "Connection closed" << endl;
+		else
+		{
+			cout << FormatLastError(dwError = WSAGetLastError(), szError) << endl;
+			break;
+
+		}
+
+		} while (strcmp(recvbuffer, DECLINE_MESSAGE) != 0 && dwError > 0);
 
 		if (strcmp(recvbuffer, DECLINE_MESSAGE) == 0)
 		{
